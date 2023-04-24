00f688fd27a9354ec1f8986c83931eca351d8c89
extrinsic-07 <erastusmatheus3@gmail.com>
2023-04-24 04:50:41 +0200

    ...

--- viewContacts.php
+++ viewContacts.php
@@ -44,8 +44,9 @@
     }
 
     // Check if there are any contacts
-    $sql = "SELECT DISTINCT contacts.*, clients.name AS client_name, clients.client_code AS client_code FROM contacts LEFT JOIN clients ON contacts.client_code = clients.client_code";
+    $sql = "SELECT contacts.contact_id, contacts.name, contacts.surname, contacts.email, contacts.linked_clients, clients.name AS client_name, clients.client_code AS client_code FROM contacts LEFT JOIN clients ON contacts.client_code = clients.client_code";
     $result = mysqli_query($conn, $sql);
+
 
     if (mysqli_num_rows($result) == 0) {
         echo "<div id = 'noContacts'>No contact(s) found.</div>";
@@ -53,9 +54,9 @@
         // Display all contacts
         echo "<h2 class='contactH'>Contact(s) table</h2>";
         echo "<table id='client-table'>";
-        echo "<tr><th>Name</th><th>Surname</th><th>Email</th><th>Linked Clients</th></tr>";
+        echo "<tr><th>ID</th><th>Name</th><th>Surname</th><th>Email</th><th>Linked Clients</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" . $row['name'] . "</td><td>" . $row['surname'] . "</td><td>" . $row['email'] . "</td><td>" . $row['client_name'] . " (" . $row['client_code'] . ")</td></tr>";
+            echo "<tr><td>" . $row['contact_id'] . "</td><td>" . $row['name'] . "</td><td>" . $row['surname'] . "</td><td>" . $row['email'] . "</td><td>" . $row['client_name'] . " (" . $row['client_code'] . ")</td></tr>";
         }
         echo "</table>";
     }
